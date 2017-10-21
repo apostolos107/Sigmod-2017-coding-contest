a5432200d94ba9ab5bf6ed1cfc45e16310d70732
Ilias Mentzelos <sdi1400106@di.uoa.gr>
2017-10-21 16:41:51 +0300

    Start the rebuild of the program with one pointer for children due to locality issues. Added parent node to the tree_node. Implemented the insert function, seems to work fine but we need the delete function to test it better. Program does not work with this main due to incompatible pointer in other functions. Uncomment every fuction you fix.

--- trie_node.c
+++ trie_node.c
@@ -15,8 +15,21 @@ trie_node* create_trie_node()
     node->is_final = 'N';
     node->current_children = 0;
     node->max_children = NUMBER_OF_CHILDREN;
-    node->children = malloc(node->max_children * sizeof(trie_node *));
+    node->word=NULL;
+    node->children = malloc(node->max_children * sizeof(trie_node));
     return node;
+}
+
+void init_trie_node(trie_node * node)
+{
+    if(node == NULL)
+        error_exit("Malloc Failure");
+
+    node->is_final = 'N';
+    node->current_children = 0;
+    node->max_children = NUMBER_OF_CHILDREN;
+    node->word=NULL;
+    node->children = malloc(node->max_children * sizeof(trie_node));
 }
 
 OK_SUCCESS insert_ngram_to_node(trie_node * node, char * ngram)
@@ -31,8 +44,8 @@ OK_SUCCESS insert_ngram_to_node(trie_node * node, char * ngram)
     {
         if(temp->current_children == 0 )
         { /*we need to add a new child cause this node has 0 of them */
-            temp->children[0] = create_trie_node();
-            temp->children[0]->word = copy_string(word);
+            init_trie_node(&temp->children[0]);
+            temp->children[0].word = copy_string(word);
             spot = 0;
             temp->current_children ++;
             printf("prosthetw tin leksi %s afou exei 0 paidia\n", word);
@@ -52,11 +65,12 @@ OK_SUCCESS insert_ngram_to_node(trie_node * node, char * ngram)
 
                 int i;
                 /*move the elements so we can make space for the new child*/
-                for (i = temp->current_children-1; i >= spot; i--)
-                    temp->children[i+1] = temp->children[i];
+                // for (i = temp->current_children-1; i >= spot; i--)
+                    memmove(&temp->children[spot+1], &temp->children[spot], (temp->current_children-spot)*sizeof(trie_node));
+                    // temp->children[i+1] = temp->children[i];
                 /*create the new child*/
-                temp->children[spot] = create_trie_node();
-                temp->children[spot]->word = copy_string(word);
+                init_trie_node(&temp->children[spot]);
+                temp->children[spot].word = copy_string(word);
                 temp->current_children ++;
 
                 printf("prosthetw tin leksi %s \n", word);
@@ -67,100 +81,100 @@ OK_SUCCESS insert_ngram_to_node(trie_node * node, char * ngram)
         word = strtok(NULL, " ");
         if (word == NULL)
         {/*end of N-Gram*/
-            temp->children[spot]->is_final = 'Y';
+            temp->children[spot].is_final = 'Y';
             return 1;
         }
         /*next word of our N-Gram*/
-        temp = temp->children[spot];
+        temp = &temp->children[spot];
     }
 }
 
-OK_SUCCESS delete_node_child(trie_node* node,int position)
-{   /* free the child */
-    free(node->children[position]->children);
-    free(node->children[position]->word);
-    free(node->children[position]);
-
-    /*fill the gap in the children array that created due to the removal*/
-    int i;
-    for(i=position+1; i<node->current_children ;i++)
-        node->children[i-1]=node->children[i];
-    node->children[node->current_children-1]=NULL;
-
-    node->current_children--;
-    return 1;
-}
-
-OK_SUCCESS anadromic_delete(trie_node* node,char* remaining)
-{
-    if(remaining==NULL)
-    {/*end of N-gram*/
-        return 2;
-    }
-    int a=0,m,position,found=0;
-    int b=node->current_children;
-    while((a <= b) && (a< node->current_children))
-    {
-        m = (a + b) /2;
-        if ( strcmp(remaining, node->children[m]->word) == 0)
-        { /*this sentence exist as far as here*/
-            position = m;
-            found =1;
-            printf("i leksi %s iparxi\n", remaining);
-            break;
-        }
-        else if( strcmp(remaining, node->children[m]->word) < 0)
-        {/*word belongs before this child*/
-            if(m == 0)break;
-            b = m-1;
-        }
-        else if( strcmp(remaining, node->children[m]->word) > 0)
-        {/*word belongs after this child*/
-            a = m+1;
-        }
-    }
-    if(found==0)
-    {/*the requested N-Grams doesn't exist */
-        printf("i leksi %s den iparxi\n", remaining);
-        return -1 ;
-    }
-
-    /*get next word of our N-Gram*/
-    char* newremaining=strtok(NULL, " ");
-    int return_value=anadromic_delete(node->children[position],newremaining);
-
-    if(return_value==1)
-    { /*It is a middle word or the first*/
-        if(node->children[position]->current_children==0 && node->children[position]->is_final!='Y')
-            /*If there are not children and It is not a final*/
-            delete_node_child(node,position);
-
-        return 1;
-    }
-    else if(return_value==2)
-    { /*Last word of Ngram*/
-        printf("I am the last: %s\n", remaining);
-        if(node->children[position]->is_final=='Y')
-        {/*It must be a final word*/
-            node->children[position]->is_final='N';
-            if(node->children[position]->current_children==0)
-            {
-                delete_node_child(node,position);
-                return 1;
-            }
-        }
-        else
-        {
-            printf("It is not a Final\n");
-            return -1;
-        }
-    }
-    else
-    {
-        return -1;
-    }
-}
-
+// OK_SUCCESS delete_node_child(trie_node* node,int position)
+// {   /* free the child */
+//     free(node->children[position]->children);
+//     free(node->children[position]->word);
+//     free(node->children[position]);
+//
+//     /*fill the gap in the children array that created due to the removal*/
+//     int i;
+//     for(i=position+1; i<node->current_children ;i++)
+//         node->children[i-1]=node->children[i];
+//     node->children[node->current_children-1]=NULL;
+//
+//     node->current_children--;
+//     return 1;
+// }
+//
+// OK_SUCCESS anadromic_delete(trie_node* node,char* remaining)
+// {
+//     if(remaining==NULL)
+//     {/*end of N-gram*/
+//         return 2;
+//     }
+//     int a=0,m,position,found=0;
+//     int b=node->current_children;
+//     while((a <= b) && (a< node->current_children))
+//     {
+//         m = (a + b) /2;
+//         if ( strcmp(remaining, node->children[m]->word) == 0)
+//         { /*this sentence exist as far as here*/
+//             position = m;
+//             found =1;
+//             printf("i leksi %s iparxi\n", remaining);
+//             break;
+//         }
+//         else if( strcmp(remaining, node->children[m]->word) < 0)
+//         {/*word belongs before this child*/
+//             if(m == 0)break;
+//             b = m-1;
+//         }
+//         else if( strcmp(remaining, node->children[m]->word) > 0)
+//         {/*word belongs after this child*/
+//             a = m+1;
+//         }
+//     }
+//     if(found==0)
+//     {/*the requested N-Grams doesn't exist */
+//         printf("i leksi %s den iparxi\n", remaining);
+//         return -1 ;
+//     }
+//
+//     /*get next word of our N-Gram*/
+//     char* newremaining=strtok(NULL, " ");
+//     int return_value=anadromic_delete(node->children[position],newremaining);
+//
+//     if(return_value==1)
+//     { /*It is a middle word or the first*/
+//         if(node->children[position]->current_children==0 && node->children[position]->is_final!='Y')
+//             /*If there are not children and It is not a final*/
+//             delete_node_child(node,position);
+//
+//         return 1;
+//     }
+//     else if(return_value==2)
+//     { /*Last word of Ngram*/
+//         printf("I am the last: %s\n", remaining);
+//         if(node->children[position]->is_final=='Y')
+//         {/*It must be a final word*/
+//             node->children[position]->is_final='N';
+//             if(node->children[position]->current_children==0)
+//             {
+//                 delete_node_child(node,position);
+//                 return 1;
+//             }
+//         }
+//         else
+//         {
+//             printf("It is not a Final\n");
+//             return -1;
+//         }
+//     }
+//     else
+//     {
+//         return -1;
+//     }
+// }
+//
 //binary search that returns the index in the array of the children
 int binary_search_kid(trie_node* master_node,char* word,int* spot_ptr_arg){
     int spot = 0;//the current spot
@@ -179,14 +193,14 @@ int binary_search_kid(trie_node* master_node,char* word,int* spot_ptr_arg){
     {
         m = (a + b) /2;
         // printf("M is %d\n", m);
-        if ( strcmp(word, master_node->children[m]->word) == 0)
+        if ( strcmp(word, master_node->children[m].word) == 0)
         { /*this sentence exist as far as here*/
             spot = m;
             found_word =1;
             printf("i leksi %s ipirxe\n", word);
             break;
         }
-        else if( strcmp(word, master_node->children[m]->word) < 0)
+        else if( strcmp(word, master_node->children[m].word) < 0)
         {/*word belongs before this child*/
             if(m == 0)
             {
@@ -196,7 +210,7 @@ int binary_search_kid(trie_node* master_node,char* word,int* spot_ptr_arg){
             b = m-1;
             spot = b;
         }
-        else if( strcmp(word, master_node->children[m]->word) > 0)
+        else if( strcmp(word, master_node->children[m].word) > 0)
         {/*word belongs after this child*/
             a = m+1;
             spot = a;
--- trie_node.h
+++ trie_node.h
@@ -11,7 +11,7 @@ typedef struct trie_node{
     char is_final;
     int current_children;
     int max_children;
-    struct trie_node** children;
+    struct trie_node* children;
 }trie_node;
 
 

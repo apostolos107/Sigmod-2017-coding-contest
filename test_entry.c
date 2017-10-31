720e3cc87fdc7c424848b97c399cf52a6c9b7f81
apostolos107 <apostolos107@gmail.com>
2017-10-31 20:20:39 +0200

    Added unit test for capitals and for reallocing

--- test_entry.c
+++ test_entry.c
@@ -5,6 +5,7 @@
 
 #include "trie.h"
 
+
 void test_insert_trie(char * sentence)
 {
     trie * mytree = malloc(sizeof(trie));
@@ -16,8 +17,61 @@ void test_insert_trie(char * sentence)
     if(!(sentence == NULL || strcmp(sentence, "")==0))
         assert(strcmp(mytree->root->children[0].word, "hey")==0);
 
-    assert(trie_clean(mytree) == 1);
+    assert(trie_clean(&mytree) == 1);
 }
+
+void test_insert_search_delete()
+{
+    trie * mytree = malloc(sizeof(trie));
+    mytree->root = init_trie();
+
+    // check the capitals
+    char sentence1[]="IS A RED CAR";
+    int result = insert_ngram(mytree,sentence1);
+    assert(result == 1);
+
+    char sentence2[]="is a red car";
+    result = insert_ngram(mytree,sentence2);
+    assert(result == 1);
+
+    char sentence3[]="Is a Red Car";
+    result = insert_ngram(mytree,sentence3);
+    assert(result == 1);
+
+    char search1[]="Is a Red Car";
+    result_of_search* search_result=search(mytree,search1);
+    assert(search_result->num_of_results==1);
+    delete_result(&search_result);
+
+    char search2[]="iS A rEd car";
+    search_result=search(mytree,search2);
+    assert(search_result->num_of_results==0);
+    delete_result(&search_result);
+
+    // check the children tables when reallocing (the children table of is needs one more)
+    int i;
+    char* sentence;
+    char* temp;
+    sentence=malloc(sizeof(char)*(NUMBER_OF_CHILDREN+5));
+    strcpy(sentence,"is ");
+    for( i = 0 ; i <= NUMBER_OF_CHILDREN ; i++ )
+    {
+        strcat(sentence,"a");
+        temp=malloc(sizeof(char)*(strlen(sentence)+1));
+        strcpy(temp,sentence);
+        result = insert_ngram(mytree,temp);
+        free(temp);
+        assert(result == 1);
+    }
+    search_result=search(mytree,sentence);
+    assert(search_result->num_of_results==1);
+    delete_result(&search_result);
+
+    free(sentence);
+    assert(trie_clean(&mytree)==1);
+    assert(mytree==NULL);
+}
+
 
 void test_insert_cases()
 {
@@ -32,6 +86,7 @@ void test_insert_cases()
  int main(void)
  {
     test_insert_cases();
+    test_insert_search_delete();
     printf("Passed the test!\n");
     return 0;
 }

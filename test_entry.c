21c6999c1a78a980a863f5eedc4f480be1c7d507
apostolos107 <apostolos107@gmail.com>
2017-10-31 20:55:32 +0200

    I added the delete at the unit test

--- test_entry.c
+++ test_entry.c
@@ -20,8 +20,8 @@ void test_insert_trie(char * sentence)
     assert(trie_clean(&mytree) == 1);
 }
 
-void test_insert_search_delete()
-{
+//init a trie with some ngrams
+trie* init_test_trie(){
     trie * mytree = malloc(sizeof(trie));
     mytree->root = init_trie();
 
@@ -47,11 +47,15 @@ void test_insert_search_delete()
     search_result=search(mytree,search2);
     assert(search_result->num_of_results==0);
     delete_result(&search_result);
+    return mytree;
+}
 
-    // check the children tables when reallocing (the children table of is needs one more)
-    int i;
+// check the children tables when reallocing (the children table of is needs one more)
+void test_realloc(trie* mytree){
+    int i,result;
     char* sentence;
     char* temp;
+    result_of_search* search_result;
     sentence=malloc(sizeof(char)*(NUMBER_OF_CHILDREN+5));
     strcpy(sentence,"is ");
     for( i = 0 ; i <= NUMBER_OF_CHILDREN ; i++ )
@@ -66,8 +70,39 @@ void test_insert_search_delete()
     search_result=search(mytree,sentence);
     assert(search_result->num_of_results==1);
     delete_result(&search_result);
-
     free(sentence);
+}
+
+//check the delete
+void test_delete(trie* mytree){
+    int result;
+    result_of_search* search_result;
+    if(NUMBER_OF_CHILDREN==0){
+        printf("The number of children is 0\n");
+        assert(NUMBER_OF_CHILDREN>0);
+    }
+
+    //add the "is" ngram
+    char sentence0[]="is";
+    result = insert_ngram(mytree,sentence0);
+    assert(result == 1);
+
+    //check the delete
+    char delete_sentence0[]="is a";
+    delete_ngram(mytree,delete_sentence0);
+    search_result=search(mytree,delete_sentence0);
+    //the results must be 1 because of the "is" ngram and because we deleted "is a"
+    // else it is should be 0 or 2 if the delete is not correct
+    assert(search_result->num_of_results==1);
+    delete_result(&search_result);
+}
+
+void test_insert_search_delete()
+{
+
+    trie* mytree=init_test_trie();
+    test_realloc(mytree);
+    test_delete(mytree);
     assert(trie_clean(&mytree)==1);
     assert(mytree==NULL);
 }
